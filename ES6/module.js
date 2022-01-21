80f77f40235b8f625e9e1b3098f884db09b23910
JonathanDev <jonathan.cardozo.dev@gmail.com>
2022-01-21 18:16:17 -0500

    Changing folder name to ES6

---
+++ ES6/module.js
@@ -0,0 +1,5 @@
+const hello = () => "hello soy un modulo asi que vivo en otro archivo";
+
+// Exportamos la funcion y asi estara disponible para otros archivos
+// export default hello; esta forma no la lee code runer
+module.exports = hello; //Usaremos esta forma para poder usar code runer
