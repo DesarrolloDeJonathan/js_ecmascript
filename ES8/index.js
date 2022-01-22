137ed6d7390937d5e674d7abd1f495489b745694
JonathanDev <jonathan.cardozo.dev@gmail.com>
2022-01-21 23:46:18 -0500

    Async Await with .try .catch ES8?

--- ES8/index.js
+++ ES8/index.js
@@ -31,3 +31,34 @@ const string = "hello";
 console.log(string.padStart(7, "hi")); // En este caso agrega 7 espacios antes la palabra hi
 //padEnd() agrega al final e indica cuantos caracteres debe contar para poner los guiones al final
 console.log(string.padEnd(12, " ----->"));
+
+// Ejemplo de una de las formas de usar Async Await
+// Tenemos una promesa para trabajar Async Await
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    // Alteramos la condicion para ver como se captura el error
+    false
+      ? setTimeout(() => resolve("Hello Promise later three seconds"), 3000)
+      : reject(new Error("Test Error"));
+  });
+};
+
+// usaremos la palabra reservada async para crear una arrow funtion que tendra la espera  (await) y con esta epera manejamos mejor el asincronismo
+const helloAsync = async () => {
+  const hello = await helloPromise();
+  // Podremos tener una serie de constantes que se iran ejecutando despues de la ejecucion anterior
+  console.log(hello);
+};
+
+helloAsync();
+
+// Forma correcta de usar async await ya que manejaremos los errores
+const otherFunction = async () => {
+  try {
+    const hello = await helloPromise();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+otherFunction();
